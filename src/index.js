5509bbadaee5945f0774d941327ecf958564dddc
ArthurMeloG <arthurdemelo2001@gmail.com>
2021-05-18 17:04:05 -0300

    (#132) Ajuste index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ import {ConfirmaLoginContaUsuario} from './modules/apiServices.js';
       localStorage.removeItem('abrirModal');
     }
     document.addEventListener("click", function (e) {
+     
+      
     //VALIDA ENTRADA EM GITHUB MILESTONES
     if (urlParts[5] == "milestones" && urlParts[2] == "github.com") {         
      
@@ -42,7 +44,7 @@ import {ConfirmaLoginContaUsuario} from './modules/apiServices.js';
           //INSTRUÇÃO PARA AUTORIZAR A ABERTURA DO MODAL
           localStorage.setItem('abrirModal', 'true')
           // var base = document.querySelectorAll("form.d-inline-block.mr-2").action;
-          location.reload(true);
+          location.reload();
      }
     }
   });
