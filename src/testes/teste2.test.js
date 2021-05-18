7020cd9ac48a06c9499a40ea59838c56bfef8d7c
JaimeJuan11 <jaime_ruan@hotmail.com>
2021-05-18 11:44:38 -0300

    Subindo correçoes nas dependencias do package.json

---
+++ src/testes/teste2.test.js
@@ -0,0 +1,11 @@
+import getFactorial from './teste2'
+
+test('getFactorial should be a function', () => {
+    expect(getFactorial).toBeInstanceOf(Function)
+})
+
+test('getFactorial return 6', () => {
+    const atual = getFactorial(3)
+    const expected = 6
+    expect(atual).toBe(expected)
+})
